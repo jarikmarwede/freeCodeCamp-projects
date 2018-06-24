8c8799036fd5f0d78658afc4d80cbe31084f7eef
Jarik Marwede <33765260+jarikmarwede@users.noreply.github.com>
2018-06-24 10:02:43 +0200

    Added load more button to hom page

--- Back End Development Certification/Voting App/public/home.js
+++ Back End Development Certification/Voting App/public/home.js
@@ -1,25 +1,30 @@
+const API_POLLS_PATH = "/api/getpolls";
+
 $(document).ready(function () {
-  displayPolls();
+  $("#load-more-btn").hide();
+  loadPolls();
+  
+  $("#load-more-btn").on("click", function() {
+    showMorePolls();
+  });
 });
 
-function displayPolls() {
-  const apiPollsPath = "/api/getpolls";
-  
-  $.getJSON(apiPollsPath, function(pollsArray) {
+function loadPolls() {
+  $.getJSON(API_POLLS_PATH, function(pollsArray) {
     let newHTML = "";
     
     for (let i = pollsArray.length - 1; i >= 0; i--) {
       let pollName = pollsArray[i]["poll-name"];
       
-      if (i < 10) {
-        newHTML += 
+      if (i > pollsArray.length - 11) {
+        newHTML = newHTML +
           "<div class=\"poll-btn\"> \
             <a class=\"poll-link text-center\" href=\"/poll/" + pollName +  "\"> \
               <h6>" + pollName + "</h6> \
             </a> \
           </div>";
       } else {
-        newHTML += 
+        newHTML = newHTML +
           "<div class=\"poll-btn hidden\"> \
             <a class=\"poll-link text-center\" href=\"/poll/" + pollName +  "\"> \
               <h6>" + pollName + "</h6> \
@@ -31,5 +36,20 @@ function displayPolls() {
       newHTML += "<h3 class=\"text-center\">No polls found!</h3>";
     }
     $("#poll-list").html(newHTML);
+    if (pollsArray.length > 10) {
+      $("#load-more-btn").show();
+    }
   });
+};
+
+function showMorePolls() {
+  const polls = $("#poll-list").children();
+  const visiblePollAmount = $("#poll-list").children(":not(.hidden)").length;
+  const startIndex = visiblePollAmount;
+  const endIndex = visiblePollAmount + 10;
+  
+  polls.slice(startIndex, endIndex).removeClass("hidden");
+  if ($("#poll-list").children(".hidden").length === 0) {
+    $("#load-more-btn").hide();
+  }
 };
