7fb8a7f8d4b5c121470796f1deb2a18220834c50
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-09-22 10:22:23 +0200

    Removed jQuery from JavaScript Calculator

--- Front End Development Certification/JavaScript Calculator/script.js
+++ Front End Development Certification/JavaScript Calculator/script.js
@@ -1,76 +1,94 @@
 function clearOutput() {
-  $("#main-output").text("0");
-  $("#full-output").text("0");
+  document.getElementById("main-output").innerText = "0";
+  document.getElementById("full-output").innerText = "0";
 }
 
 function clearLast() {
-  if ($("#full-output").text().length <= 1) {
+  const fullOutput = document.getElementById("full-output");
+  const mainOutput = document.getElementById("main-output");
+
+  if (fullOutput.innerText.length <= 1) {
     return null;
   }
-  $("#full-output").text($("#full-output").text().slice(0, $("#full-output").text().length - 1));
-  $("#main-output").text($("#full-output").text()[$("#full-output").text().length - 1]);
+  fullOutput.innerText = fullOutput.innerText.slice(0, fullOutput.innerText.length - 1);
+  mainOutput.innerText = fullOutput.innerText[fullOutput.innerText.length - 1];
 }
 
 function addNumber(number) {
-  if (number == "." && $("#main-output").text().indexOf(".") != -1) {
+  const fullOutput = document.getElementById("full-output");
+  const mainOutput = document.getElementById("main-output");
+
+  if (number == "." && mainOutput.innerText.indexOf(".") != -1) {
     return null;
-  } else if ($("#full-output").text().indexOf("=") != -1) {
+  } else if (fullOutput.innerText.indexOf("=") != -1) {
     clearOutput();
   }
-  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  if (numbers.indexOf($("#main-output").text()[$("#main-output").length -1]) != -1 || $("#main-output").text()[$("#main-output").length -1] == "." || ($("#main-output").text().length == 1 && numbers.indexOf($("#main-output").text()[0]) != -1)) {
-    var newText = $("#main-output").text() + number;
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  if (numbers.indexOf(mainOutput.innerText[mainOutput.children.length - 1]) != -1 || mainOutput.innerText[mainOutput.children.length - 1] == "." || (mainOutput.innerText.length == 1 && numbers.indexOf(mainOutput.innerText[0]) != - 1)) {
+    let newText = mainOutput.innerText + number;
     while (newText[0] == "0" && newText.length > 1 && newText[1] != ".") {
       newText = newText.slice(1);
     }
-    $("#main-output").text(newText);
+    mainOutput.innerText = newText;
   } else {
-    $("#main-output").text(number);
+    mainOutput.innerText = number;
   }
-  $("#full-output").text($("#full-output").text() + number);
-  while ($("#full-output").text()[0] == "0" && $("#full-output").text().length > 1 && numbers.indexOf($("#full-output").text()[1]) != -1) {
-    $("#full-output").text($("#full-output").text().slice(1));
+  fullOutput.innerText += number;
+  while (fullOutput.innerText[0] == "0" && fullOutput.innerText.length > 1 && numbers.indexOf(fullOutput.innerText[1]) != -1) {
+    fullOutput.innerText = fullOutput.innerText.slice(1);
   }
 }
 
 function addSymbol(symbol) {
-  var operations = ["+", "-", "*", "/"];
-  if (operations.indexOf($("#main-output").text()[0]) != -1) {
+  const fullOutput = document.getElementById("full-output");
+  const mainOutput = document.getElementById("main-output");
+  const operations = ["+", "-", "*", "/"];
+
+  if (operations.indexOf(mainOutput.innerText[0]) != -1) {
     return null;
-  } else if ($("#full-output").text().indexOf("=") != -1) {
+  } else if (fullOutput.innerText.indexOf("=") != -1) {
     clearOutput();
   }
-  $("#main-output").text(symbol);
-  $("#full-output").text($("#full-output").text() + symbol);
+  mainOutput.innerText = symbol;
+  fullOutput.innerText += symbol;
 }
 
 function evaluate() {
-  var result = eval($("#full-output").text());
-  $("#main-output").text(result);
-  $("#full-output").text($("#full-output").text() + "=" + result);
+  const fullOutput = document.getElementById("full-output");
+  const mainOutput = document.getElementById("main-output");
+
+  var result = eval(fullOutput.innerText);
+  mainOutput.innerText = result;
+  fullOutput.innerText += "=" + result;
 }
 
-$("document").ready(function() {
-  $("#ac-btn").on("click", function() {
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("ac-btn").addEventListener("click", function() {
     clearOutput();
   });
-  $("#delete-btn").on("click", function() {
+  document.getElementById("delete-btn").addEventListener("click", function() {
     clearLast();
   });
-  $(".number-btn").on("click", function() {
-    var number = $(this).text();
-    addNumber(number);
-  });
-  $(".operation-btn").on("click", function() {
-    var operationSymbol = $(this).text();
-    if (operationSymbol == "÷") {
-      operationSymbol = "/";
-    } else if (operationSymbol == "X") {
-      operationSymbol = "*";
-    }
-    addSymbol(operationSymbol);
-  });
-  $("#equal-btn").on("click", function() {
+  for (const current of document.getElementsByClassName("number-btn")) {
+    current.addEventListener("click", function() {
+      var number = this.innerText;
+      addNumber(number);
+    })
+  }
+  for (const current of document.getElementsByClassName("operation-btn")) {
+    current.addEventListener("click", function() {
+      let operationSymbol = this.innerText;
+
+      if (operationSymbol == "÷") {
+        operationSymbol = "/";
+      } else if (operationSymbol == "X") {
+        operationSymbol = "*";
+      }
+      addSymbol(operationSymbol);
+    });
+  }
+  document.getElementById("equal-btn").addEventListener("click", function() {
     evaluate();
   })
 })
