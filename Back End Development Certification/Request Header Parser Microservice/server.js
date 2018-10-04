be978fb2bb0250cd205fa4f43ce3594244129a89
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-10-04 08:28:08 +0200

    Changed var to const

--- Back End Development Certification/Request Header Parser Microservice/server.js
+++ Back End Development Certification/Request Header Parser Microservice/server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.use(express.static('public'));
 
-app.get("/", function (request, response) {
+app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/api/whoami", function (req, res) {
-  var responseJSON = {
+app.get("/api/whoami", (req, res) => {
+  const responseJSON = {
     "ipaddress": req.get("x-forwarded-for").split(",")[0],
     "language": req.get("accept-language").split(",")[0],
     "software": req.get("user-agent").split("(")[1].split(")")[0]
@@ -16,6 +16,6 @@ app.get("/api/whoami", function (req, res) {
   res.send(responseJSON);
 });
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, () => {
+  console.log('The app is listening on port ' + listener.address().port);
 });
