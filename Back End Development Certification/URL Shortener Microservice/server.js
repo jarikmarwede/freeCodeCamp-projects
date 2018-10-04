caa7026afd81544daaae2f591d72835cd8945847
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-10-04 12:18:31 +0200

    Changed var to let or const and converted anonymous functions to =>

--- Back End Development Certification/URL Shortener Microservice/server.js
+++ Back End Development Certification/URL Shortener Microservice/server.js
@@ -1,27 +1,28 @@
-var mongodb = require("mongodb");
-var express = require('express');
+const mongodb = require("mongodb");
+const express = require('express');
 
-var MongoClient = mongodb.MongoClient;
-var databaseUrl = process.env.DATABASE_URL;
-var app = express();
+const MongoClient = mongodb.MongoClient;
+const databaseUrl = process.env.DATABASE_URL;
+const app = express();
 
 app.use(express.static('public'));
-app.get("/", function (request, response) {
+
+app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/:id", function (req, res) {
+app.get("/:id", (req, res) => {
   if (parseInt(req.params.id) != NaN && Number.isInteger(parseInt(req.params.id))) {
-    var urlId = parseInt(req.params.id);
+    const urlId = parseInt(req.params.id);
 
-    MongoClient.connect(databaseUrl, async function(err, client) {
+    MongoClient.connect(databaseUrl, async (err, client) => {
       if (err) {
         console.log("Error connecting to database: " + err);
       } else {
-        var db = client.db("url-shortener-microservice");
-        var collection = db.collection("shortened-urls");
-        var idResult = await collection.find({"id": urlId}).toArray();
-        
+        const db = client.db("url-shortener-microservice");
+        const collection = db.collection("shortened-urls");
+        const idResult = await collection.find({"id": urlId}).toArray();
+
         if (idResult.length > 0) {
           res.redirect(idResult[0]["original_url"]);
           client.close();
@@ -36,29 +37,29 @@ app.get("/:id", function (req, res) {
   }
 });
 
-app.get("/new/*?", async function (req, res) {
+app.get("/new/*?", async (req, res) => {
   if (req.params[0].search(/^http(s)?:\/\/(.)+(\.){1}(.)+/gi) != -1) {
-    var originalUrl = req.params[0];
-    var shortUrl = req.protocol + '://' + req.get('host') + "/";
+    const originalUrl = req.params[0];
+    let shortUrl = req.protocol + '://' + req.get('host') + "/";
 
     MongoClient.connect(databaseUrl, async function(err, client) {
       if (err) {
         console.log("Error connecting to database: " + err);
       } else {
-        var db = client.db("url-shortener-microservice");
-        var collection = db.collection("shortened-urls");
+        const db = client.db("url-shortener-microservice");
+        const collection = db.collection("shortened-urls");
 
-        var urlResult = await collection.find({"original_url": originalUrl}).toArray();
+        const urlResult = await collection.find({"original_url": originalUrl}).toArray();
         if (urlResult.length > 0) {
           shortUrl += urlResult[0]["id"];
         } else {
-          var allDocuments = await collection.find({}).toArray();
+          const allDocuments = await collection.find({}).toArray();
           collection.insert({"original_url": originalUrl, "id": allDocuments.length});
           shortUrl += allDocuments.length;
         }
         client.close();
-        
-        var responseJSON = {
+
+        const responseJSON = {
           "original_url": originalUrl,
           "short_url": shortUrl
         };
@@ -76,6 +77,6 @@ function invalidUrl(res) {
   res.send({"error": "URL invalid"});
 };
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, () => {
+  console.log('The app is listening on port ' + listener.address().port);
 });
