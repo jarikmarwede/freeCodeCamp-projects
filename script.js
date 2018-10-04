633518d16f54e18df32b448a2b6e656e0e8b3ec3
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-10-04 15:55:28 +0200

    Fixed links to projects that were moved to a different Certification

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const FRONT_END_PROJECTS = [
   {name: "JavaScript Calculator", link: "./Front End Development Certification/JavaScript Calculator/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Front%20End%20Development%20Certification/JavaScript%20Calculator"},
   {name: "Local Wheather", link: "./Front End Development Certification/Local Weather/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Front%20End%20Development%20Certification/Local%20Weather"},
-  {name: "Personal Portfolio Webpage", link: "./Front End Development Certification/Personal Portfolio Webpage/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Front%20End%20Development%20Certification/Personal%20Portfolio%20Webpage"},
   {name: "Pomodoro Clock", link: "./Front End Development Certification/Pomodoro Clock/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Front%20End%20Development%20Certification/Pomodoro%20Clock"},
   {name: "Random Quote Machine", link: "./Front End Development Certification/Random Quote Machine/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Front%20End%20Development%20Certification/Random%20Quote%20Machine"},
   {name: "Simon Game", link: "./Front End Development Certification/Simon Game/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Front%20End%20Development%20Certification/Simon%20Game"},
   {name: "Tic Tac Toe Game", link: "./Front End Development Certification/Tic Tac Toe Game/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Front%20End%20Development%20Certification/Tic%20Tac%20Toe%20Game"},
-  {name: "Tribute Page", link: "./Front End Development Certification/Tribute Page/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Front%20End%20Development%20Certification/Tribute%20Page"},
   {name: "Twitch API", link: "./Front End Development Certification/Twitch API/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Front%20End%20Development%20Certification/Twitch%20API"},
   {name: "Wikipedia Viewer", link: "./Front End Development Certification/Wikipedia Viewer/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Front%20End%20Development%20Certification/Wikipedia%20Viewer"}
 ]
@@ -19,7 +17,9 @@ const BACK_END_PROJECTS = [
   {name: "Voting App", link: "https://jm-voting-app.glitch.me", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Back%20End%20Development%20Certification/Voting%20App"}
 ]
 const RESPONSIVE_WEB_DESIGN_PROJECTS = [
-  {name: "Survey Form", link: "./Responsive Web Design Certification/Survey Form/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Responsive%20Web%20Design%20Certification/Survey%20Form"}
+  {name: "Personal Portfolio Webpage", link: "./Responsive Web Design Certification/Personal Portfolio Webpage/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Responsive%20Web%20Design%20Certification/Personal%20Portfolio%20Webpage"},
+  {name: "Survey Form", link: "./Responsive Web Design Certification/Survey Form/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Responsive%20Web%20Design%20Certification/Survey%20Form"},
+  {name: "Tribute Page", link: "./Responsive Web Design Certification/Tribute Page/index.html", src: "https://github.com/jarikmarwede/freeCodeCamp-projects/tree/master/Responsive%20Web%20Design%20Certification/Tribute%20Page"}
 ]
 const CERTIFICATIONS = [
   {projects: FRONT_END_PROJECTS, container_id: "front-end-projects"},
