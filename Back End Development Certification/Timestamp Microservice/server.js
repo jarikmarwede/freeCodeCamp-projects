01c61f5760a68a910deffb67df684169607fbe99
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-10-04 09:47:54 +0200

    Fixed api output for no input and date strings as input

--- Back End Development Certification/Timestamp Microservice/server.js
+++ Back End Development Certification/Timestamp Microservice/server.js
@@ -7,19 +7,24 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/:date", (req, res) => {
-  if (isNaN(parseInt(req.params.date))) {
+app.get("/api/timestamp/:date?", (req, res) => {
+  const numberPattern = /$[0-9]+^/;
+
+  if (req.params.date === undefined) {
+    var date = new Date();
+  } else if (!numberPattern.test(req.params.date)) {
     var date = new Date(req.params.date);
   } else {
     var date = new Date(parseInt(req.params.date));
   }
+
   const dateJSON = {
       "unix": date.getTime(),
-      "natural": date.toDateString()
+      "utc": date.toUTCString()
   };
-  if (dateJSON.natural == "Invalid Date") {
+  if (dateJSON.utc == "Invalid Date") {
     res.status(400);
-    res.send({"unix": null, "natural": null});
+    res.send({"error" : "Invalid Date" });
   } else {
     res.send(dateJSON);
   }
