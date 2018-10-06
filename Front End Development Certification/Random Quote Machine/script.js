67da0c39da167f4ee5fab58a0b9b944619b6b325
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2018-10-06 16:17:05 +0200

    Replaced all jQuery functionality wiht vanilla Javascript

--- Front End Development Certification/Random Quote Machine/script.js
+++ Front End Development Certification/Random Quote Machine/script.js
@@ -1,34 +1,46 @@
-function getNewQuote() {
-  $.ajax( {
-      url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-      success: function(json) {updateQuote(json)},
-      cache: false,
-	  crossDomain: true
-    });
+async function retrieveQuote() {
+  const response = await fetch("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", {cache: "no-store"});
+  const reponseData = await response.json();
+  return reponseData;
 }
 
-function updateQuote(json) {
-  $("#quote-text").fadeOut(0);
-  $("#quote-author").fadeOut(0);
-  $("#quote-source").fadeOut(0);
-  quote = json[0];
-  $("#quote-text").html(quote.content);
-  $("#quote-author").html("- " + quote.title);
-  if (typeof quote.custom_meta !== "undefined" && typeof quote.custom_meta.Source !== "undefined") {
-    $("#quote-source").html("Source: " + quote.custom_meta.Source);
+async function getNewQuote() {
+  toggleQuoteVisibility();
+  const responseData = await retrieveQuote();
+  const quote = responseData[0];
+  const quoteElement = document.getElementById("quote-text");
+  const authorElement = document.getElementById("quote-author");
+  const sourceElement = document.getElementById("quote-source");
+
+  quoteElement.innerHTML = quote.content;
+  authorElement.innerHTML = "- " + quote.title;
+  if (quote.custom_meta !== undefined && quote.custom_meta.Source !== undefined) {
+    sourceElement.innerHTML = "Source: " + quote.custom_meta.Source;
   } else {
-    $("#quote-source").text("");
-  };
-  var twitter_url = encodeURI("https://twitter.com/intent/tweet?text=" + $("#quote-text").text() + " " + $("#quote-author").text());
-  $("#twitter-btn").attr("href", twitter_url);
-  $("#quote-text").fadeIn("slow");
-  $("#quote-author").fadeIn("slow");
-  $("#quote-source").fadeIn("slow");
+    sourceElement.innerHTML = "";
+  }
+  var twitter_url = encodeURI("https://twitter.com/intent/tweet?text=" + quoteElement.textContent + " " + authorElement.textContent);
+  document.getElementById("twitter-btn").href = twitter_url;
+  toggleQuoteVisibility();
 }
 
-$(document).ready(function() {
+function toggleQuoteVisibility() {
+  const quoteElement = document.getElementById("quote-text");
+  const authorElement = document.getElementById("quote-author");
+  const sourceElement = document.getElementById("quote-source");
+
+  if (quoteElement.style.display == "none") {
+    quoteElement.style.display = "block";
+    authorElement.style.display = "block";
+    sourceElement.style.display = "block";
+  } else {
+    quoteElement.style.display = "none";
+    authorElement.style.display = "none";
+    sourceElement.style.display = "none";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
   getNewQuote();
-  $("#random-btn").on("click", function() {
-    getNewQuote();
-  });
+  document.getElementById("random-btn").addEventListener("click", getNewQuote);
 });
