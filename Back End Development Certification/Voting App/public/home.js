24ea7fd61c0a680a0772cef55dd4099015590b39
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2019-01-20 13:47:08 +0100

    Updated home page to handlebars

--- Back End Development Certification/Voting App/public/home.js
+++ Back End Development Certification/Voting App/public/home.js
@@ -1,55 +1,22 @@
-const API_POLLS_PATH = "/api/getpolls";
-
-function loadPolls() {
-  $.getJSON(API_POLLS_PATH, (pollsArray) => {
-    let newHTML = "";
-
-    for (let i = pollsArray.length - 1; i >= 0; i--) {
-      let pollName = pollsArray[i]["poll-name"];
-
-      if (i > pollsArray.length - 11) {
-        newHTML = newHTML +
-          "<div class=\"poll-btn\"> \
-            <a class=\"poll-link text-center\" href=\"/poll/" + pollName +  "\"> \
-              <h6>" + pollName + "</h6> \
-            </a> \
-          </div>";
-      } else {
-        newHTML = newHTML +
-          "<div class=\"poll-btn hidden\"> \
-            <a class=\"poll-link text-center\" href=\"/poll/" + pollName +  "\"> \
-              <h6>" + pollName + "</h6> \
-            </a> \
-          </div>";
-      }
-    }
-    if (newHTML == "") {
-      newHTML += "<h3 class=\"text-center\">No polls found!</h3>";
-    }
-    $("#poll-list").html(newHTML);
-    if (pollsArray.length > 10) {
-      $("#load-more-btn").show();
-    }
-  });
-};
-
 function showMorePolls() {
-  const polls = $("#poll-list").children();
-  const visiblePollAmount = $("#poll-list").children(":not(.hidden)").length;
+  const polls = document.getElementById("poll-list").children;
+  let visiblePollAmount = polls.length;
+  for (const poll of polls) {
+    if (poll.hidden) {
+      visiblePollAmount--;
+    }
+  }
   const startIndex = visiblePollAmount;
   const endIndex = visiblePollAmount + 10;
 
-  polls.slice(startIndex, endIndex).removeClass("hidden");
-  if ($("#poll-list").children(".hidden").length === 0) {
-    $("#load-more-btn").hide();
+  if (endIndex >= polls.length) {
+    document.getElementById("load-more-btn").hidden = true;
   }
-};
+  for (let index = startIndex; index < endIndex && index < polls.length; index++) {
+    polls[index].hidden = false;
+  }
+}
 
-$(document).ready(() => {
-  $("#load-more-btn").hide();
-  loadPolls();
-
-  $("#load-more-btn").on("click", () => {
-    showMorePolls();
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("load-more-btn").addEventListener("click", showMorePolls);
 });
