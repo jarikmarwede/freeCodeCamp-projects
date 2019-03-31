6b642566e243e3901fd47eaa877d1223c75f12ad
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2019-03-31 20:38:21 +0200

    Added API endpoints to change and delete user data

--- Back_End_Development_Certification/Voting_App/routes/api.js
+++ Back_End_Development_Certification/Voting_App/routes/api.js
@@ -65,12 +65,12 @@ router.post("/newpoll", async (request, response) => {
 
     const success = await server.createNewPoll(pollName, username, answers);
     if (success) {
-      response.redirect("/poll/" + pollName);
+      response.status(201).redirect("/poll/" + pollName);
     } else {
-      response.redirect("back");
+      response.status(500).redirect("back");
     }
   } else {
-    response.redirect("/signup");
+    response.status(401).redirect("/signup");
   }
 });
 
@@ -114,6 +114,49 @@ router.delete("/poll/:poll", async (request, response) => {
   if (request.middlewareData.loggedIn && ownsPoll) {
     await server.deletePoll(pollName);
     response.sendStatus(200);
+  } else {
+    response.sendStatus(401);
+  }
+});
+
+router.post("/user/:username", async (request, response) => {
+  if (request.middlewareData.loggedIn && request.params.username === request.cookies.username) {
+    const success = await server.updateUserData(request.params.username, {username: request.body.username, email: request.body.email});
+    if (success) {
+      response.status(200).redirect("back");
+    } else {
+      response.status(500).redirect("back");
+    }
+  } else {
+    response.sendStatus(401);
+  }
+});
+
+router.post("/user/:username/change_password", async (request, response) => {
+  if (request.middlewareData.loggedIn && request.params.username === request.cookies.username) {
+    if (request.body.newPassword === request.body.newPassword2) {
+      const success = await server.changePassword(request.params.username, request.body.newPassword);
+      if (success) {
+        response.status(200).redirect("back");
+      } else {
+        response.status(500).redirect("back");
+      }
+    } else {
+      response.sendStatus(400);
+    }
+  } else {
+    response.sendStatus(401);
+  }
+});
+
+router.delete("/user/:username", async (request, response) => {
+  if (request.middlewareData.loggedIn && request.params.username === request.cookies.username) {
+    const success = await server.deleteUser(request.params.username);
+    if (success) {
+      response.sendStatus(200);
+    } else {
+      response.sendStatus(500);
+    }
   } else {
     response.sendStatus(401);
   }
