ad47fb6905f806f43ececcc477ddd6c64f2388d5
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2019-04-06 18:18:08 +0200

    Factored out clearCookies function into helpers.js

--- Back_End_Development_Certification/Voting_App/public/helpers.js
+++ Back_End_Development_Certification/Voting_App/public/helpers.js
@@ -2,3 +2,8 @@ function getCookie(name) {
   const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
   if (match) return match[2];
 }
+
+function clearCookies() {
+  document.cookie = "session=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
+  document.cookie = "username=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
+}
--- Back_End_Development_Certification/Voting_App/public/navbar.js
+++ Back_End_Development_Certification/Voting_App/public/navbar.js
@@ -1,8 +1,3 @@
-function clearCookies() {
-  document.cookie = "session=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
-  document.cookie = "username=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("logout-btn").addEventListener("click", () => {
     clearCookies();
