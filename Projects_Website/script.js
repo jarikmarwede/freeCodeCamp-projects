15db4810324ea0159828b1ab6d522487488c2e66
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2020-04-09 18:38:08 +0200

    Put navigation bar into custom element

--- Projects_Website/script.js
+++ Projects_Website/script.js
@@ -1,7 +1,9 @@
 import CERTIFICATIONS from "./certifications.js";
+import "./app-nav/app-nav.js";
 import "./certification-section/certification-section.js";
 
 function loadCertifications() {
+  document.getElementsByTagName("app-nav")[0].setAttribute("certifications", JSON.stringify(CERTIFICATIONS).replace(/"/g, "'"));
   for (const certification of CERTIFICATIONS) {
     const descriptionString = certification.description.replace(/"/g, "'");
     const projectsString = JSON.stringify(certification.projects).replace(/"/g, "'");
