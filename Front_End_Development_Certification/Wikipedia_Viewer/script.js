c5b661d6264936aa10245665ae9258751aa3d622
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2020-04-10 19:29:41 +0200

    Put search result cards into custom element of Wikipedia Viewer

--- Front_End_Development_Certification/Wikipedia_Viewer/script.js
+++ Front_End_Development_Certification/Wikipedia_Viewer/script.js
@@ -1,60 +1,53 @@
+import "./search-result-card/search-result-card.js";
+
 let pageLimit = 10;
 
+function newSearch() {
+  pageLimit = 10;
+  wikipediaSearch(document.getElementById("search-input").value).then();
+}
+
 async function wikipediaSearch(string, pageLimit=10) {
-  const responseData = await requestSearch(string, pageLimit);
-  showSearchResults(responseData);
+  showSearchResults(await requestSearch(string, pageLimit));
 }
 
 async function requestSearch(string, pageLimit) {
   const requestUrl = "https://en.wikipedia.org/w/api.php?" + ["origin=*", "format=json", "action=query", "list=search", "srsearch=" + encodeURI(string), "srlimit=" + pageLimit].join("&");
   const response = await fetch(requestUrl);
-  const responseData = await response.json();
-  return responseData;
+  return response.json();
 }
 
 function showSearchResults(json) {
   document.getElementById("search-results-container").innerHTML = "";
-  const pages = json.query.search;
-  const totalhits = json.query.searchinfo.totalhits;
-  document.getElementById("search-results-container").innerHTML += "<p class=\"total-matches\">Total matches: " + totalhits + "</p>";
-  for (let i = 0; i < pages.length; i++) {
-    const page = pages[i]
-    const url = "https://en.wikipedia.org/?curid=" + page.pageid
-    const snippet = page.snippet
-    const title = page.title
-    addSearchResult(title, snippet, url);
+  const pages = json["query"]["search"];
+  const totalHits = json["query"]["searchinfo"]["totalhits"];
+  document.getElementById("search-results-container").innerHTML += "<p class=\"total-matches\">Total matches: " + totalHits + "</p>";
+  for (const page of pages) {
+    const url = "https://en.wikipedia.org/?curid=" + page["pageid"];
+    addSearchResult(page["title"], page["snippet"], url);
   }
   if (pageLimit < 50) {
     document.getElementById("search-results-container").innerHTML += "<button class=\"load-more-btn\" id=\"load-more-btn\">Load more</button>";
-  document.getElementById("load-more-btn").addEventListener("click", () => {
-    if (pageLimit <= 40) {
-      pageLimit += 10;
-    }
-    wikipediaSearch(document.getElementById("search-input").value, pageLimit);
-  });
+    document.getElementById("load-more-btn").addEventListener("click", () => {
+      if (pageLimit <= 40) {
+        pageLimit += 10;
+      }
+      wikipediaSearch(document.getElementById("search-input").value, pageLimit).then();
+    });
   }
 }
 
 function addSearchResult(title, snippet, url) {
-  html =
-    '<div class="search-result"> \
-      <a class="search-result-link" href="' + url +  '"> \
-        <h3 class="search-result-title">' + title + '</h3> \
-        <p class="search-result-snippet">' + snippet + '...</p> \
-      </a> \
-    </div>'
-  document.getElementById("search-results-container").innerHTML += html;
+  const snippetString = snippet.replace(/"/g, "'");
+  document.getElementById("search-results-container").innerHTML += `
+    <search-result-card url="${url}" title="${title}" snippet="${snippetString}"></search-result-card>
+  `;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("search-btn").addEventListener("click", () => {
-    pageLimit = 10;
-    wikipediaSearch(document.getElementById("search-input").value)
-  });
+  document.getElementById("search-btn").addEventListener("click", newSearch);
   document.getElementById("search-input").addEventListener("keypress", (event) => {
-    if (event.which == 13) {
-      pageLimit = 10;
-      wikipediaSearch(document.getElementById("search-input").value);
-    }
+    if (event.key === "Enter")
+      newSearch();
   });
 })
