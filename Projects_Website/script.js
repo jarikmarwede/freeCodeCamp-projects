3562494f2d6eaa82b674e566d2d4477ea8db19a4
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2020-04-12 17:20:55 +0200

    Use slot for passing certification description instead of attribute

--- Projects_Website/script.js
+++ Projects_Website/script.js
@@ -5,10 +5,11 @@ import "./certification-section/certification-section.js";
 function loadCertifications() {
   document.getElementsByTagName("app-nav")[0].setAttribute("certifications", JSON.stringify(CERTIFICATIONS).replace(/"/g, "'"));
   for (const certification of CERTIFICATIONS) {
-    const descriptionString = certification.description.replace(/"/g, "'");
     const projectsString = JSON.stringify(certification.projects).replace(/"/g, "'");
     document.getElementById("certifications").innerHTML += `
-      <certification-section id="${certification.container_id}" name="${certification.name}" description="${descriptionString}" projects="${projectsString}"></certification-section>
+      <certification-section id="${certification.container_id}" name="${certification.name}" projects="${projectsString}">
+        <div slot="description">${certification.description}</div>
+      </certification-section>
     `;
   }
 }
