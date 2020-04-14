4c209596d3219097a20a6fb4020533e81bda67a0
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2020-04-14 17:14:15 +0200

    Put main content into app-main custom element

--- Projects_Website/script.js
+++ Projects_Website/script.js
@@ -1,17 +1,9 @@
 import CERTIFICATIONS from "./certifications.js";
 import "./app-nav/app-nav.js";
-import "./certification-section/certification-section.js";
+import "./app-main/app-main.js";
 
 function loadCertifications() {
   document.getElementsByTagName("app-nav")[0].setAttribute("certifications", JSON.stringify(CERTIFICATIONS).replace(/"/g, "'"));
-  for (const certification of CERTIFICATIONS) {
-    const projectsString = JSON.stringify(certification.projects).replace(/"/g, "'");
-    document.getElementById("certifications").innerHTML += `
-      <certification-section id="${certification.container_id}" name="${certification.name}" projects="${projectsString}">
-        <div slot="description">${certification.description}</div>
-      </certification-section>
-    `;
-  }
 }
 
 window.addEventListener(
