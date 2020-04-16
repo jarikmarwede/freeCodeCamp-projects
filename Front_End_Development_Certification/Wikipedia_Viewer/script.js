b5e442a0846da640ab3e4f58d3119d04856968c6
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2020-04-16 11:46:49 +0200

    Add fade-in animation to Wikipedia Viewer search-result-cards

--- Front_End_Development_Certification/Wikipedia_Viewer/script.js
+++ Front_End_Development_Certification/Wikipedia_Viewer/script.js
@@ -12,36 +12,32 @@ async function wikipediaSearch(string, pageLimit=10) {
 }
 
 async function requestSearch(string, pageLimit) {
-  const requestUrl = "https://en.wikipedia.org/w/api.php?" + ["origin=*", "format=json", "action=query", "list=search", "srsearch=" + encodeURI(string), "srlimit=" + pageLimit].join("&");
+  const requestUrl = "https://en.wikipedia.org/w/api.php?" + ["origin=*", "format=json", "action=query", "list=search", "srsearch=" + encodeURI(string), "sroffset=" + pageLimit - 10, "srlimit=" + pageLimit].join("&");
   const response = await fetch(requestUrl);
   return response.json();
 }
 
 function showSearchResults(json) {
-  document.getElementById("search-results-container").innerHTML = "";
+  if (pageLimit <= 10)
+    document.getElementById("search-results-container").innerHTML = "";
   const pages = json["query"]["search"];
   const totalHits = json["query"]["searchinfo"]["totalhits"];
-  document.getElementById("search-results-container").innerHTML += "<p class=\"total-matches\">Total matches: " + totalHits + "</p>";
+  document.getElementById("total-matches").innerText = "Total matches: " + totalHits;
   for (const page of pages) {
     const url = "https://en.wikipedia.org/?curid=" + page["pageid"];
     addSearchResult(page["title"], page["snippet"], url);
   }
-  if (pageLimit < 50) {
-    document.getElementById("search-results-container").innerHTML += "<button class=\"load-more-btn\" id=\"load-more-btn\">Load more</button>";
-    document.getElementById("load-more-btn").addEventListener("click", () => {
-      if (pageLimit <= 40) {
-        pageLimit += 10;
-      }
-      wikipediaSearch(document.getElementById("search-input").value, pageLimit).then();
-    });
-  }
+  document.getElementById("load-more-btn").hidden = pageLimit >= 50;
 }
 
 function addSearchResult(title, snippet, url) {
-  const snippetString = snippet.replace(/"/g, "'");
-  document.getElementById("search-results-container").innerHTML += `
-    <search-result-card url="${url}" title="${title}" snippet="${snippetString}"></search-result-card>
+  const searchResultCard = document.createElement("search-result-card");
+  searchResultCard.url = url;
+  searchResultCard.innerHTML = `
+    <span slot="title">${title}</span>
+    <span slot="snippet">${snippet}</span>
   `;
+  document.getElementById("search-results-container").appendChild(searchResultCard);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -49,5 +45,11 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("search-input").addEventListener("keypress", (event) => {
     if (event.key === "Enter")
       newSearch();
+  });
+  document.getElementById("load-more-btn").addEventListener("click", () => {
+    if (pageLimit <= 40) {
+      pageLimit += 10;
+    }
+    wikipediaSearch(document.getElementById("search-input").value, pageLimit).then();
   });
 })
--- Front_End_Development_Certification/Wikipedia_Viewer/search-result-card/search-result-card.js
+++ Front_End_Development_Certification/Wikipedia_Viewer/search-result-card/search-result-card.js
@@ -1,27 +1,39 @@
+const template = document.createElement("template");
+template.innerHTML = `
+  <link rel="stylesheet" href="./search-result-card/search-result-card.css">
+  <a href="#">
+    <h3>
+      <slot name="title"></slot>
+    </h3>
+    <p id="snippet">
+      <slot name="snippet"></slot>
+      ...
+    </p>
+  </a>
+`;
+
 class SearchResultCard extends HTMLElement {
   constructor() {
     super();
 
-    const shadow = this.attachShadow({mode: "open"});
-    shadow.innerHTML = `
-      <link rel="stylesheet" href="./search-result-card/search-result-card.css">
-      <a href="${this.url}">
-        <h3>${this.title}</h3>
-        <p id="snippet">${this.snippet}...</p>
-      </a>
-    `;
+    this.attachShadow({mode: "open"});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  static get observedAttributes() {
+    return ["url"];
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "url") {
+      this.shadowRoot.querySelector("a").href = newValue;
+    }
   }
 
   get url() {
     return this.getAttribute("url");
   }
-
-  get title() {
-    return this.getAttribute("title");
-  }
-
-  get snippet() {
-    return this.getAttribute("snippet").replace(/'/g, "\"");
+  set url(url) {
+    this.setAttribute("url", url);
   }
 }
 
