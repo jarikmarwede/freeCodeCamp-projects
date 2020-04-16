2917d81f948df6bcd1382451667198a6e46f73b9
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2020-04-16 15:31:02 +0200

    Add loading icon to Request Header Parser Microservice

--- Apis_And_Microservices_Certification/Request_Header_Parser_Microservice/script.js
+++ Apis_And_Microservices_Certification/Request_Header_Parser_Microservice/script.js
@@ -1,6 +1,8 @@
 async function getAPIResponse() {
   const API_URL = "https://jm-freecodecamp-projects.herokuapp.com/request-header-parser-microservice/whoami";
+  document.querySelector(".code-wrapper img").hidden = false;
   const response = await fetch(API_URL);
+  document.querySelector(".code-wrapper img").hidden = true;
   document.getElementById("api-response").innerText = JSON.stringify(await response.json());
 }
 
