1239dac5319e35b5608d816897d331c3b613962d
Jarik Marwede <contact@jarikmarwede.com>
2022-12-19 17:49:11 +0100

    Add serverless function for querying Image Search API

--- Back_End_Development_Certification/Image_Search_Abstraction_Layer/server.js
+++ Back_End_Development_Certification/Image_Search_Abstraction_Layer/server.js
@@ -1,49 +1,8 @@
 import express from "express";
 const app = express();
 import mongodb from "mongodb";
-import fetch from "node-fetch";
 const MongoClient = mongodb.MongoClient;
 const databasePath = process.env.IMAGE_SEARCH_ABSTRACTION_LAYER_DATABASE_URL || "mongodb://localhost/image-search-abstraction-layer";
-
-app.get("/imagesearch/*?", async (req, res) => {
-  const searchString = req.params[0];
-  const offset = req.query.offset || 1;
-  const APICallString = "https://contextualwebsearch-websearch-v1.p.mashape.com/api/Search/ImageSearchAPI?count=" + offset * 10 + "&q=" + searchString + "&autocorrect=false";
-  const apiPromise = fetch(APICallString, {
-    headers: {
-      "X-RapidAPI-Key": process.env.RAPID_API_KEY
-    }
-  }).catch(() => {});
-
-  const dbClient = new MongoClient(databasePath);
-  try {
-    await dbClient.connect();
-  } catch (error) {
-    console.log("Error connecting to database: " + error);
-    res.sendStatus(500);
-  }
-  const db = dbClient.db("image-search-abstraction-layer");
-  const recentSearchesCollection = db.collection("latest-image-searches");
-  const date = new Date();
-  const timestampInsertion = recentSearchesCollection.insertOne({"term": searchString, "when": date.toISOString()});
-
-  let recentSearches = await recentSearchesCollection.find({}).toArray();
-  if (recentSearches.length > 10) {
-    recentSearches = recentSearches.slice(1, 11);
-    await recentSearchesCollection.deleteMany({});
-    await recentSearchesCollection.insertMany(recentSearches);
-  }
-  await timestampInsertion;
-  dbClient.close().then();
-
-  const apiResponse = await apiPromise;
-  if (apiResponse === undefined) {
-    return res.sendStatus(502);
-  }
-  const responseData = await apiResponse.json();
-  const images = responseData.value.slice(offset * 10 - 10, offset * 10);
-  await res.status(200).json(images);
-});
 
 app.get("/latest/imagesearch/", async (req, res) => {
   const dbClient = new MongoClient(databasePath);
