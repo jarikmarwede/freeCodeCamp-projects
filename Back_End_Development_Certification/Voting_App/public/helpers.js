d5015e6f4a054ff358a4237a9642325b9058eb66
Jarik Marwede <contact@jarikmarwede.com>
2025-06-15 10:41:41 +0200

    Add secure attribute to cookies

--- Back_End_Development_Certification/Voting_App/public/helpers.js
+++ Back_End_Development_Certification/Voting_App/public/helpers.js
@@ -4,6 +4,6 @@ function getCookie(name) {
 }
 
 function clearCookies() {
-  document.cookie = "session=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
-  document.cookie = "username=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
+  document.cookie = "session=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;secure";
+  document.cookie = "username=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;secure";
 }
